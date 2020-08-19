af4097d7a884375ff20deba722b521def234825a
Cissou13 <cyrille.fabre@gmail.com>
2020-08-19 23:42:52 +0200

    Home + Form

--- techchallenge-web-dev-starter-forked/script.js
+++ techchallenge-web-dev-starter-forked/script.js
@@ -4,8 +4,15 @@ setInterval(function getTime() {
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
   hours = (hours < 10 ? ' 0' : ' ') + hours;
-  minutes = (minutes < 10 ? '0' : '') + minutes;
+  minutes = (minutes < 10 ? ':0' : ':') + minutes;
   seconds = (seconds < 10 ? ':0' : ':') + seconds;
   let myHour = document.getElementById('my-hour');
-  myHour.textContent = hours + 'h' + minutes + seconds;
+  myHour.textContent = 'Time : ' + hours + minutes + seconds;
 }, 1000);
+
+const message = 'Thanks you for contacting us, we will come back to you soon !';
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert(message);
+});
