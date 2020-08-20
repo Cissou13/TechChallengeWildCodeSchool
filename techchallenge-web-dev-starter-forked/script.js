0fe8425139c5c2a93891c62b8f00ca6430a198ae
Cissou13 <cyrille.fabre@gmail.com>
2020-08-20 02:12:53 +0200

    Ajout Royal Family

--- techchallenge-web-dev-starter-forked/script.js
+++ techchallenge-web-dev-starter-forked/script.js
@@ -7,7 +7,7 @@ setInterval(function getTime() {
   minutes = (minutes < 10 ? ':0' : ':') + minutes;
   seconds = (seconds < 10 ? ':0' : ':') + seconds;
   let myHour = document.getElementById('my-hour');
-  myHour.textContent = 'Time : ' + hours + minutes + seconds;
+  myHour.textContent = 'Current time in Colchis : ' + hours + minutes + seconds;
 }, 1000);
 
 const message = 'Thanks you for contacting us, we will come back to you soon !';
